7de903449b469f0f97be1bf08e5f8e56b4e5cbd4
Darko Izgarevic <darko.izgarevic@oryxgaming.com>
2022-06-09 09:54:51 +0200

    full commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Pozivnica" src="./assets/pozivnica.jpeg">
+    <p>Molimo potvrdite Vaš dolazak najkasnije do 15.07.2022. godine putem sledeće forme: </p>
+    <contact-form />
+  </div>
+</template>
+
+<script>
+import ContactForm from './components/ContactForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+p {
+  font-size: 28px;
+}
+
+img {
+  width: 700px;
+  padding-bottom: 60px;
+}
+</style>
